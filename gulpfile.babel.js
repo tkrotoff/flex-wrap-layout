03625346458d6380f5013c1c90be5e64a36876bf
Tanguy Krotoff <tkrotoff@gmail.com>
2017-08-26 09:59:25 +0200

    Remove old IE support

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,6 @@
 import gulp from 'gulp';
 import debug from 'gulp-debug';
 import sass from 'gulp-sass';
-import autoprefixer from 'gulp-autoprefixer';
 import sourcemaps from 'gulp-sourcemaps';
 import del from 'del';
 import browserSync from 'browser-sync';
@@ -14,7 +13,6 @@ gulp.task('styles', () =>
     .pipe(sourcemaps.init())
     // See input-group: button is one pixel too small in bootstrap-sass (but not when using Less) https://github.com/twbs/bootstrap-sass/issues/595
     .pipe(sass({precision: 10}))
-    .pipe(autoprefixer({browsers: ['last 2 versions']}))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('examples'))
     .pipe(debug({title: 'styles:'}))
