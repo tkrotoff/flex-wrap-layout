49b1aafac19236c3dc474c4a5d78e2ba168f7d47
Tanguy Krotoff <tkrotoff@gmail.com>
2017-08-29 11:10:26 +0200

    Simplify toggleBorders()

--- src/toggleBorders.ts
+++ src/toggleBorders.ts
@@ -1,21 +1,8 @@
-// See Changing CSS Values with Javascript http://stackoverflow.com/questions/566203
-function addGlobalCSS(selector: string, css: string) {
-  // Loop through all styles
-  for (var i = 0; i < document.styleSheets.length; i++) {
-    const styleSheet = document.styleSheets[i] as CSSStyleSheet;
-    styleSheet.insertRule(
-      selector + ' {' + css + '}',
-      styleSheet.cssRules.length
-    );
-  }
-}
-
-function toggleBorders(checkbox: HTMLInputElement) {
-  if (checkbox.checked) {
-    addGlobalCSS('[layout=row]', 'border: 1px solid');
-    addGlobalCSS('[layout=column]', 'border: 1px solid');
+function toggleBorders() {
+  const classList = document.body.classList;
+  if (classList.contains('showBorders')) {
+    classList.remove('showBorders');
   } else {
-    addGlobalCSS('[layout=row]', 'border: 0');
-    addGlobalCSS('[layout=column]', 'border: 0');
+    classList.add('showBorders');
   }
 }
