5da2a26e2eb1fbb55afe1c1b9184022fd597fb6a
Tanguy Krotoff <tkrotoff@gmail.com>
2019-03-25 01:20:30 +0100

    Improve detectRowWrap() example

--- examples/detectRowWrap.ts
+++ examples/detectRowWrap.ts
@@ -10,10 +10,10 @@ declare global {
     // FIXME ESLint is incorrect here
     /* eslint-disable no-undef */
 
-    DetectRowWrap: typeof DetectRowWrapController;
+    DetectRowWrapController: typeof DetectRowWrapController;
   }
 }
 
 // See [Calling webpacked code from outside (HTML script tag)](https://stackoverflow.com/a/34358513/990356)
 window.Borders = Borders;
-window.DetectRowWrap = DetectRowWrapController;
+window.DetectRowWrapController = DetectRowWrapController;
