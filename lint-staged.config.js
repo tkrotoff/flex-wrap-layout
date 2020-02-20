5a2173b257ad793b4bc78ee0c57a72b5dd4f3c77
Tanguy Krotoff <tkrotoff@gmail.com>
2020-02-20 14:01:05 +0100

    Run lint-staged tsc on all files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,8 +3,7 @@ module.exports = {
   '*.{js,ts,tsx}': 'eslint --cache',
 
   // FIXME https://github.com/microsoft/TypeScript/issues/27379
-  '*.{ts,tsx}':
-    'tsc --noEmit --esModuleInterop --jsx preserve --strict --noUnusedLocals --noUnusedParameters --noImplicitReturns --noFallthroughCasesInSwitch --forceConsistentCasingInFileNames',
+  '*.{ts,tsx}': () => 'tsc',
 
   '*.scss': 'stylelint --cache'
 };
