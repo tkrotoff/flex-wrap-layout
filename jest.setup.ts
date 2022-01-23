5ef8912b1f6861fd1ed684a94c4f1a86bfff0624
Tanguy Krotoff <tkrotoff@gmail.com>
2022-01-23 10:49:09 +0100

    No need to catch unhandledRejection with Node.js 16

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,9 +2,3 @@
 import assert from 'assert';
 
 console.assert = assert;
-
-// [Event: 'unhandledRejection'](https://nodejs.org/api/process.html#process_event_unhandledrejection)
-// [Bluebird Error management configuration](http://bluebirdjs.com/docs/api/error-management-configuration.html)
-process.on('unhandledRejection', (reason: Error | any, _promise: Promise<any>) => {
-  throw reason;
-});
